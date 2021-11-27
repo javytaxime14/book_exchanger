28980d2af52a222ce5df17f94c5c1469d006dddb
Javiera Hidalgo <javiera_56@hotmail.com>
2021-11-27 20:17:25 -0300

    configuring admin panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= stub active_admin
 //= require rails-ujs
 //= require activestorage
 //= require jquery
